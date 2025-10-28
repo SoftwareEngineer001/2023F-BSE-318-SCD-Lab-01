2739a8facf3b88e904343fe0e15ddf5f35be8990
Muhammad Talha <talhadilawar88@gmail.com>
2025-10-28 23:45:44 +0500

    Update Lab01.java

--- Lab01.java
+++ Lab01.java
@@ -3,8 +3,9 @@ package Lab01;
 public class Lab01 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Java Programming Language ");
 
 	}
 
 }
+
